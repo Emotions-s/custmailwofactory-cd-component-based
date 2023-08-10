e5f148746d8702ca72d339ebc3a5e983b0453015
Emotions <estskywalker@gmail.com>
2023-08-11 00:20:18 +0700

    fix(customer) : add factory design pattern

---
+++ src/customermailapplication/CustomerFactory.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package customermailapplication;
+
+/**
+ *
+ * @author emotions
+ */
+public class CustomerFactory {
+
+    public static Customer createCustomer(String type) {
+        Customer customer = null;
+        switch (type) {
+            case "Regular":
+                customer = new RegularCustomer();
+                break;
+            case "Mountain":
+                customer = new MountainCustomer();
+                break;
+            case "Delinquent":
+                customer = new DelinquentCustomer();
+                break;
+        }
+        return customer;
+    }
+}
